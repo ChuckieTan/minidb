287d8e70f0abf828c2f5f2d276853d0dc7e5b136
Chuckie Tan <tsq1105@gmail.com>
2021-10-11 10:58:42 +0800

    add debug mode

--- main.cpp
+++ main.cpp
@@ -7,14 +7,23 @@
 #include <iostream>
 #include <string>
 
+// For debug, turn private method to public
+// #ifdef DEBUG
+// #define protected public
+// #define private public
+// #endif
+
 int main(int argc, char *argv[]) {
+    #ifdef DEBUG
+    std::cout << "debug mode" << std::endl;
+    #endif
     minidb::MinidbStrings::print_string(
         minidb::MinidbStrings::StringType::info);
     auto input = minidb::Input();
     input.readInputFromStream(std::cin);
     minidb::Lexer lexer(input.buffer);
     minidb::Parser parser(lexer);
-    std::cout << (parser.selectStatement()? 1: 0) << std::endl;
+    std::cout << (parser.selectStatement()? "true": "false") << std::endl;
     for (auto token = lexer.getNextToken();
          token.tokenType != minidb::TokenType::END &&
          token.tokenType != minidb::TokenType::ILLEGAL;
